3e12e8bf38a39bf5fa91c8c09365a0831c5ea2c5
amejin <ywc959@gmail.com>
2026-04-29 16:43:50 +0900

    v1.1.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     host_permissions: ['https://cdn.jsdelivr.net/*'],
     permissions: ['tabs', 'activeTab','storage'], // activeTab 允许你在点击插件图标时获取当前页信息
     name: '日语划词注音',
-    version: "1.0.0",
+    version: "1.1.0",
   },
   runner:{
     startUrls: ['https://note.com/kashimura_pr/n/n4c2e1c0e5ccb#4f162dc3-21f8-45d5-80e9-7e003bae578e'],
